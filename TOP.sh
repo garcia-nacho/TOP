a2b98fa87182ddc67b28311bdbace0abad81adaa
Nacho Garcia <garcia.nacho@gmail.com>
2023-03-10 13:20:55 +0100

    Update TOP.sh

--- TOP.sh
+++ TOP.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 source activate top_nf
 
-nextflow ${CONDA_PREFIX}/bin/TOP.nf --readsfolder "${1}" --krakenDB ${CONDA_PREFIX}/krakenDB
+wget -O ${CONDA_PREFIX}/bin/TOP.nf https://github.com/garcia-nacho/TOP/raw/master/TOP.nf
+wget -O ${CONDA_PREFIX}/bin/nextflow.config https://github.com/garcia-nacho/TOP/raw/master/nextflow.config
+
+nextflow ${CONDA_PREFIX}/bin/TOP.nf --readsfolder "${1}" --krakenDB ${KRAKENDB}
+
 rm -rf ./work
 
 conda deactivate
