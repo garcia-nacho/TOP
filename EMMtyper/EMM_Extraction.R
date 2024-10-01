d9021fd9ffb435bc29d4aa9a6d458a305b145e5a
garcia-nacho <garcia.nacho@gmail.com>
2024-10-01 08:01:56 +0200

    EMM gene extractor script included

---
+++ EMMtyper/EMM_Extraction.R
@@ -0,0 +1,20 @@
+library(seqinr)
+#setwd("/media/nacho/Data/temp/toptest/TopValidation/emm/3sep2024")
+fastas.list<-list.files(pattern = ".fasta")
+results<-list.files(pattern ="emm_results_blast", recursive = TRUE )
+#blastn -out 2265046-GA-24GB002298_merged_clean_contigs_emm_results_blast.tab -outfmt '6 qseqid sseqid qstart qend sstart send slen qlen length mismatch evalue' -query /Data/2265046-GA-24GB002298_merged_clean_contigs.fasta -db /emmdb07022024/emmdb.tfa
+
+
+sq<-list()
+for (i in 1:length(results)) {
+  res<-read.csv(results[i], sep = "\t", header = FALSE)
+  res<-res[which(res$V11==min(res$V11))[1],]
+  fastas<-read.fasta(fastas.list[grep(gsub("_.*","",res$V1[1]),fastas.list)])
+  fastas<-fastas[which(names(fastas)==res$V1[1])]
+  start<-min(res$V3,res$V4)
+  end<-max(res$V3,res$V4)+320
+  sq<-c(sq,list(unlist(fastas)[c(start:end)]))
+  names(sq)[length(sq)]<-gsub("_.*","",res$V1[1])
+}
+
+write.fasta(sq,"EMM_seqs_extended.fa", names = paste("EMM_",names(sq),sep = ""))
